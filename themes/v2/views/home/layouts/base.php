30e9d29c39a969735dc1034d7cc495ae35abbf5a
zhangtao <754710007@qq.com>
2017-05-31 13:56:42 +0800

    优化

--- themes/v2/views/home/layouts/base.php
+++ themes/v2/views/home/layouts/base.php
@@ -20,6 +20,7 @@
     <script type="text/javascript" src="<?=Yii::app()->theme->baseUrl?>/static/home/js/org.1487311756.js" data-main="<?=$this->banner?'baseMain':'indexMain'?>"></script>
     <title><?php echo $this->pageTitle; ?></title>
     <style type="text/css">
+        a[href="http://www.uemo.net"]{display:none}
         #sitecontent{
             padding-top: 86px
         }
@@ -80,54 +81,6 @@
         </div>
     </div>
     <div id="sitecontent">
-        
-        <div id="navMini">
-            <div id="navMiniTable">
-                <div id="navMiniCell">
-                    <ul class="nav">
-                        <li class="navitem"><a href="http://mo005-1400.mo5.line1.uemo.net/" target="_self">首页</a></li>
-                        <li class="navitem"><a href="http://mo005-1563.mo5.line1.jsmo.xin/" target="_blank">黑色单屏版</a></li>
-                        <li class="navitem"><a href="http://mo005-1400.mo5.line1.uemo.net/list/id/11111/" target="_self">业务服务</a></li>
-                        <li class="navitem"><a href="http://mo005-1400.mo5.line1.uemo.net/list/id/11061/" target="_self">项目案例</a></li>
-                        <li class="navitem"><a href="javascript:;" target="">关于我们<i class="fa fa-angle-down"></i></a>
-                            <ul class="subnav">
-                                <li><a href="http://mo005-1400.mo5.line1.uemo.net/page/about/" target="_self">集团简介</a></li>
-                                <li><a href="http://mo005-1400.mo5.line1.uemo.net/list/id/11283/" target="_self">合作伙伴</a></li>
-                            </ul>
-                        </li>
-                        <li class="navitem"><a href="http://mo005-1400.mo5.line1.uemo.net/list/id/11067/" target="_self">资讯中心</a></li>
-                        <li class="navitem"><a class="active" href="http://mo005-1400.mo5.line1.uemo.net/page/contact/" target="_self">联系我们</a></li>
-                        <li class="search-but" style=" opacity:0">
-                            <a href="#">
-                                <i class="fa fa-search" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <script>
-        $(function() {
-            $('#navWrapper .search_but').click(function() {
-                $("#search").show().animate({
-                    left: 0,
-                    opacity: 1
-                }, 500);
-                $("#navWrapper").hide();
-                $("#headTop").hide();
-
-            });
-
-            $('#search .s_close').click(function() {
-                $("#search").show().animate({
-                    left: $("#search").width() * 1.2,
-                    opcity: 0
-                }, 500);
-                $("#navWrapper").show();
-                $("#headTop").show();
-            });
-        });
-        </script>
     <?=$content?>
     </div>
     <div id="footer">
